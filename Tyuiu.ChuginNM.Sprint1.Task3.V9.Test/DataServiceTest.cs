19b06b3499bfe05ddc39b173d46c07036610b7d0
CDOOR <CDOOR@CHUGIN>
2025-09-29 18:27:37 +0500

    Task3

---
+++ Tyuiu.ChuginNM.Sprint1.Task3.V9.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ChuginNM.Sprint1.Task3.V9.Lib;
+
+namespace Tyuiu.ChuginNM.Sprint1.Task3.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 150;
+            Assert.AreEqual(2.5, ds.ConvertMinutesToHours(x));
+        }
+    }
+}
---
+++ Tyuiu.ChuginNM.Sprint1.Task3.V9/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ChuginNM.Sprint1.Task3.V9.Lib;
+
+namespace Tyuiu.ChuginNM.Sprint1.Task3.V9
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Чугин Н. М. | АСОиУб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Чугин Никита Максимович | АСОиУб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+            Console.WriteLine("Введите время в секундах: ");
+            x = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            int hour = Convert.ToInt16(ds.ConvertMinutesToHours(x) - ds.ConvertMinutesToHours(x) % 1);
+            int minute = Convert.ToInt16(ds.ConvertMinutesToHours(x)%1*60);
+            Console.WriteLine($"{x} минут - это {hour} ч. {minute} минут");
+            Console.ReadKey();
+        }
+    }
+}
