04ea39a48f10c5cb5bb40b77e2374424a05a6e7e
CDOOR <CDOOR@CHUGIN>
2025-09-30 19:51:09 +0500

    Task4

--- Tyuiu.ChuginNM.Sprint1.Task4.V14.Lib/DataService.cs
+++ Tyuiu.ChuginNM.Sprint1.Task4.V14.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.ChuginNM.Sprint1.Task4.V14.Lib
     {
         public double Calculate(double x, double y)
         {
-            return (Math.Sqrt(7 + Math.Abs(x - y))) / (3 * x * Math.Pow(y, 2));
+            return Math.Round((Math.Sqrt(7 + Math.Abs(x - y))) / (3 * x * Math.Pow(y, 2)), 3);
         }
     }
 }
