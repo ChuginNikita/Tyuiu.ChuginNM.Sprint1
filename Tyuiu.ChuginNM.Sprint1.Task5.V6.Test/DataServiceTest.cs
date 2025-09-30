be99cce16a25a91fdfe5ca14fbb06294fbcee257
CDOOR <CDOOR@CHUGIN>
2025-09-30 21:01:50 +0500

    Task5

---
+++ Tyuiu.ChuginNM.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ChuginNM.Sprint1.Task5.V6.Lib;
+
+namespace Tyuiu.ChuginNM.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            int x = 10;
+
+            Assert.AreEqual(3, ds.Calculate(x));
+
+        }
+    }
+}
--- Tyuiu.ChuginNM.Sprint1.Task5.V6/Program.cs
+++ Tyuiu.ChuginNM.Sprint1.Task5.V6/Program.cs
@@ -1,43 +1,38 @@
-﻿using Tyuiu.ChuginNM.Sprint1.Task4.V14.Lib;
+﻿using Tyuiu.ChuginNM.Sprint1.Task5.V6.Lib;
 
-namespace Tyuiu.ChuginNM.Sprint1.Task4.V14
+namespace Tyuiu.ChuginNM.Sprint1.Task5.V6
 {
+
     public class Program
     {
         static void Main(string[] args)
         {
-            DataService ds = new DataService();
 
+            DataService ds = new DataService();
             Console.Title = "Спринт #1 | Выполнил: Чугин Н. М. | АСОиУб-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
             Console.WriteLine("* Тема: Class Math                                                        *");
-            Console.WriteLine("* Задание #4                                                              *");
-            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #6                                                             *");
             Console.WriteLine("* Выполнил: Чугин Никита Максимович | АСОиУб-25-1                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
-            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
-            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу: Пусть k – целое    *");
+            Console.WriteLine("* от 1 до 365. Присвоить целой переменной n значение 1,2,...,7            *");
+            Console.WriteLine("* в зависимости от того, на какой день недели приходится k-й день года.   *");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
             Console.WriteLine("***************************************************************************");
 
             int x;
-            Console.WriteLine("Введите первое число: ");
+            Console.WriteLine("Введите число: ");
             x = Convert.ToInt16(Console.ReadLine());
-
-            int y;
-            Console.WriteLine("Введите второе число: ");
-            y = Convert.ToInt16(Console.ReadLine());
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine(Math.Round(ds.Calculate(x, y), 3));
-
-            
+            Console.WriteLine(ds.Calculate(x));
 
         }
     }
