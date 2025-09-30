19aa0acde41bc06d93c55d3b8bb48e8c731957fc
CDOOR <CDOOR@CHUGIN>
2025-09-30 19:49:53 +0500

    Task4

---
+++ Tyuiu.ChuginNM.Sprint1.Task4.V14.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.ChuginNM.Sprint1.Task4.V14.Lib
+{
+    public class DataService : ISprint1Task4V14
+    {
+        public double Calculate(double x, double y)
+        {
+            return (Math.Sqrt(7 + Math.Abs(x - y))) / (3 * x * Math.Pow(y, 2));
+        }
+    }
+}
---
+++ Tyuiu.ChuginNM.Sprint1.Task4.V14.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ChuginNM.Sprint1.Task4.V14.Lib;
+
+namespace Tyuiu.ChuginNM.Sprint1.Task4.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 6, y = 7;
+            Assert.AreEqual(0.003, Math.Round(ds.Calculate(x,y),3));
+        }
+    }
+}
